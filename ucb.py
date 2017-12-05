b3dc705405b39a63cc1df1130267442d1cc55695
Tushar <tushar.169107181@muj.manipal.edu>
2017-12-05 19:12:53 +0530

    Finding the Max Upper Bound till here

--- ucb.py
+++ ucb.py
@@ -5,3 +5,34 @@ Spyder Editor
 This is a temporary script file.
 """
 
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import math
+dataset = pd.read_csv('Ads_CTR_Optimisation.csv')
+
+N=10000 #number of users
+d=10 #number of ads
+
+numbers_of_selection = [0] * d
+sums_of_rewards = [0] * d
+
+ads_selected = []
+
+
+for n in range(N):
+    ad=0
+    max_upper_bound = 0 
+    for i in range(d):
+        average_reward = sums_of_rewards[i]/ numbers_of_selection[i]
+        delta_i = math.sqrt(3/2 * math.log(n+1) / numbers_of_selection[i] )
+        upper_bound = average_reward + delta_i
+        if upper_bound > max_upper_bound:
+            max_upper_bound = upper_bound
+            ad = i
+            
+        
+        
+        
+    
+    
